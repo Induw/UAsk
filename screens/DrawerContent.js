16b9250a0e522eab77719c91dbb6b34199ae15f0
eshan10x <eshankavishka10@gmail.com>
2021-02-02 12:24:26 +0530

    screen updated

--- screens/DrawerContent.js
+++ screens/DrawerContent.js
@@ -23,12 +23,12 @@ export function DrawerContent(props) {
                                 size={50}
                             />
                             <View style={{marginLeft:15, flexDirection:'column'}}>
-                                <Title style={styles.title}>Eshan</Title>
-                                <Caption style={styles.caption}>Kavishka</Caption>
+                                <Title style={styles.title}>UAsk</Title>
+                                <Caption style={styles.caption}></Caption>
                             </View>
                         </View>
 
-                        <View style={styles.row}>
+                        {/* <View style={styles.row}>
                             <View style={styles.section}>
                                 <Paragraph style={[styles.paragraph, styles.caption]}>80</Paragraph>
                                 <Caption style={styles.caption}>Following</Caption>
@@ -37,7 +37,7 @@ export function DrawerContent(props) {
                                 <Paragraph style={[styles.paragraph, styles.caption]}>1000K</Paragraph>
                                 <Caption style={styles.caption}>Followers</Caption>
                             </View>
-                        </View>
+                        </View> */}
                     </View>
 
                     <Drawer.Section style={styles.drawerSection}>
@@ -102,19 +102,6 @@ export function DrawerContent(props) {
                     </Drawer.Section>
                 </View>
             </DrawerContentScrollView>
-            <Drawer.Section style={styles.bottomDrawerSection}>
-                <DrawerItem 
-                    icon={({color, size}) => (
-                        <Icon 
-                        name="exit-to-app" 
-                        color={color}
-                        size={size}
-                        />
-                    )}
-                    label="Sign Out"
-                    onPress={() => console.log('XXXXXXXXXXXXXX')}
-                />
-            </Drawer.Section>
         </View>
     );
 }
