7de2e5cae1217ee2b56636f95b82a14adee46484
Ravindu Induwara <ravinduinduwara@gmail.com>
2021-02-23 13:00:03 +0530

    Added some comments to the code

--- Models/SpeechRecognition/SpeechToText.py
+++ Models/SpeechRecognition/SpeechToText.py
@@ -1,20 +1,19 @@
-# import library
+# importing speech recognition library
 import speech_recognition as sr
 
-# Initialize recognizer class (for recognizing the speech)
+# Initializing the recognizer class (to recognize voice speech)
 r = sr.Recognizer()
 
-# Reading Microphone as source
-# listening the speech and store in audio_text variable
-
+# Reading Microphone as source using pyaudio
+# listening to the speech and store in a variable called audio
 with sr.Microphone() as source:
     print("Talk ....")
-    audio_text = r.listen(source)
+    audio = r.listen(source)
     print("Time over, thanks")
     # recognize_() method will throw a request error if the API is unreachable, exception handling is used here
 
     try:
-        # using google speech recognition
-        print("Text: " + r.recognize_google(audio_text))
+        # using google speech recognition api to identify words in the audio
+        print("Text: " + r.recognize_google(audio))
     except:
         print("Sorry, I did not get that")
