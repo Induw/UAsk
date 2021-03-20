ad183348f909fe0060e7575e95849d1ed1b8eae9
Ravindu Induwara <ravinduinduwara@gmail.com>
2021-03-20 21:48:20 +0530

    Update QuestionAnswerScreen.js
    Added the Voice to Text Functionality after removing the sample screen that was implemented before.

--- FrontEnd/screens/QuestionAnswerScreen.js
+++ FrontEnd/screens/QuestionAnswerScreen.js
@@ -1,22 +1,10 @@
-import React, { useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import { View, StyleSheet, Image, Text, TouchableOpacity, SafeAreaView, FlatList } from 'react-native';
+import { ScrollView } from 'react-native-gesture-handler';
 import Icon from 'react-native-vector-icons/Ionicons';
 import Icons from 'react-native-vector-icons/MaterialCommunityIcons';
 
-const DATA = [
-  {
-    id: 'bd7acbea-c1b1-46c2-aed5-3ad53abb28ba',
-    title: 'First Item',
-  },
-  {
-    id: '3ac68afc-c605-48d3-a4f8-fbd91aa97f63',
-    title: 'Second Item',
-  },
-  {
-    id: '58694a0f-3da1-471f-bd96-145571e29d72',
-    title: 'Third Item',
-  },
-]
+import Voice from 'react-native-voice';
 
 const Item = ({ title }) => (
   <View style={styles.item}>
@@ -26,49 +14,175 @@ const Item = ({ title }) => (
 
 const QuestionAnswerScreen = (props) => {
 
+  const [pitch, setPitch] = useState('');
+  const [error, setError] = useState('');
+  const [end, setEnd] = useState('');
+  const [started, setStarted] = useState('');
+  const [results, setResults] = useState([]);
+  const [partialResults, setPartialResults] = useState([]);
+
+  useEffect(() => {
+    //Setting callbacks for the process status
+    Voice.onSpeechStart = onSpeechStart;
+    Voice.onSpeechEnd = onSpeechEnd;
+    Voice.onSpeechError = onSpeechError;
+    Voice.onSpeechResults = onSpeechResults;
+    Voice.onSpeechPartialResults = onSpeechPartialResults;
+    Voice.onSpeechVolumeChanged = onSpeechVolumeChanged;
+
+    return () => {
+      //destroy the process after switching the screen
+      Voice.destroy().then(Voice.removeAllListeners);
+    };
+  }, []);
+
+  const onSpeechStart = (e) => {
+    //Invoked when .start() is called without error
+    console.log('onSpeechStart: ', e);
+    setStarted('√');
+  };
+
+  const onSpeechEnd = (e) => {
+    //Invoked when SpeechRecognizer stops recognition
+    console.log('onSpeechEnd: ', e);
+    setEnd('√');
+  };
+
+  const onSpeechError = (e) => {
+    //Invoked when an error occurs.
+    console.log('onSpeechError: ', e);
+    setError(JSON.stringify(e.error));
+  };
+
+  const onSpeechResults = (e) => {
+    //Invoked when SpeechRecognizer is finished recognizing
+    console.log('onSpeechResults: ', e);
+    setResults(e.value);
+  };
+
+  const onSpeechPartialResults = (e) => {
+    //Invoked when any results are computed
+    console.log('onSpeechPartialResults: ', e);
+    setPartialResults(e.value);
+  };
+
+  const onSpeechVolumeChanged = (e) => {
+    //Invoked when pitch that is recognized changed
+    console.log('onSpeechVolumeChanged: ', e);
+    setPitch(e.value);
+  };
+
+  const startRecognizing = async () => {
+    //Starts listening for speech for a specific locale
+    try {
+      await Voice.start('en-US');
+      setPitch('');
+      setError('');
+      setStarted('');
+      setResults([]);
+      setPartialResults([]);
+      setEnd('');
+    } catch (e) {
+      //eslint-disable-next-line
+      console.error(e);
+    }
+  };
+
+  const stopRecognizing = async () => {
+    //Stops listening for speech
+    try {
+      await Voice.stop();
+    } catch (e) {
+      //eslint-disable-next-line
+      console.error(e);
+    }
+  };
+
+  const cancelRecognizing = async () => {
+    //Cancels the speech recognition
+    try {
+      await Voice.cancel();
+    } catch (e) {
+      //eslint-disable-next-line
+      console.error(e);
+    }
+  };
+
+  const destroyRecognizer = async () => {
+    //Destroys the current SpeechRecognizer instance
+    try {
+      await Voice.destroy();
+      setPitch('');
+      setError('');
+      setStarted('');
+      setResults([]);
+      setPartialResults([]);
+      setEnd('');
+    } catch (e) {
+      //eslint-disable-next-line
+      console.error(e);
+    }
+  };
+
   useEffect(() => {
     // Update the document title using the browser API
-    console.log(props+"--------------------------------------------------------");
-  },[]);
+    console.log(props + "--------------------------------------------------------");
+  }, []);
 
-  const renderItem = ({ item }) => (
-    <Item title={item.title} />
+  return (
+    <SafeAreaView style={styles.container}>
+      <View style={styles.imgView}>
+        <Image style={styles.img} source={{ uri: props.route.params.uri }} ></Image>
+      </View>
+      <View style={styles.QuestionAnswerBox}>
+        <View style={styles.wrap}>
+          <Text>
+            {/* <ScrollView> */}
+              {
+              <Text
+                style={styles.textStyle}>
+                {results[0] + "?"}
+              </Text>
+              
+            }
+            {console.log(results[0]+ "?")}
+
+            {/* {results.map((result, index) => {
+              console.log(index + "index --------------------------");
+              console.log(result);
+            return (
+              <Text
+                key={`result-${index}`}
+                
+                style={styles.textStyle}>
+                {result}
+              </Text>
+            );
+          })} */}
+            {/* </ScrollView> */}
+          </Text>
+        </View>
+      </View>
+      <View style={styles.bottomBar}>
+        <TouchableOpacity style={styles.capture}>
+          < Icon name="camera-outline" onPress={() => { props.navigation.navigate('Home') }} size={36} color='#264CAD' />
+        </TouchableOpacity>
+        <TouchableOpacity onPress={startRecognizing} style={styles.capture}>
+          <Icon name="mic-circle-outline" size={70} color='#264CAD' />
+        </TouchableOpacity>
+        <TouchableOpacity onPress={() => flipCamera()} style={styles.capture}>
+          <Icons name='keyboard' size={36} type='material' color='#264CAD' />
+        </TouchableOpacity>
+      </View>
+    </SafeAreaView>
   );
-
-    return (
-      <SafeAreaView style={styles.container}>
-          <View style={styles.imgView}>
-            <Image style={styles.img} source={{uri:props.route.params.uri}} ></Image>
-          </View>
-          <View style={styles.QuestionAnswerBox}>
-            <View style={styles.wrap}>
-            <FlatList
-              data={DATA}
-              renderItem={renderItem}
-              keyExtractor={item => item.id}
-            />
-            </View>
-          </View>
-          <View style={styles.bottomBar}>
-              <TouchableOpacity     style={styles.capture}>
-              < Icon name="camera-outline" onPress={() => {props.navigation.navigate('Home')}} size={36} color='#264CAD' />
-            </TouchableOpacity>
-              <TouchableOpacity  onPress={() => takePicture(props)} style={styles.capture}>
-              <Icon name="mic-circle-outline" size={70} color='#264CAD' />
-            </TouchableOpacity>
-              <TouchableOpacity  onPress={() => flipCamera()} style={styles.capture}>
-            <Icons name='keyboard' size={36} type='material' color='#264CAD' />
-            </TouchableOpacity>
-          </View>
-      </SafeAreaView>
-    );
 };
 
 export default QuestionAnswerScreen;
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1, 
+    flex: 1,
     alignItems: 'center',
     backgroundColor: '#264CAD',
   },
@@ -91,8 +205,12 @@ const styles = StyleSheet.create({
     opacity: 0.6,
   },
   wrap: {
-    margin: 5,
-    borderRadius: 10 
+    margin: 10,
+    borderRadius: 10,
+    backgroundColor: '#fff',
+    width: '100%'
+
+
   },
   capture: {
     flex: 0,
@@ -103,14 +221,14 @@ const styles = StyleSheet.create({
     marginVertical: 5
   },
   bottomBar: {
-    flex: 0, 
-    flexDirection: 'row', 
-    backgroundColor: '#fff', 
-    borderRadius: 100, 
-    marginTop: 20, 
+    flex: 0,
+    flexDirection: 'row',
+    backgroundColor: '#fff',
+    borderRadius: 100,
+    marginTop: 20,
     opacity: 0.8,
   },
-   item: {
+  item: {
     backgroundColor: '#f9c2ff',
     padding: 20,
     marginVertical: 8,
@@ -118,5 +236,8 @@ const styles = StyleSheet.create({
   },
   title: {
     fontSize: 32,
+  },
+  textStyle: {
+    padding: 12,
   },
 });
