98ab1964d10725b72d19b274c68c8d78d4a2c474
Janith lasindu <64950088+janithlazz@users.noreply.github.com>
2021-04-17 08:01:08 +0530

    help screen update

--- FrontEnd/screens/HelpScreen.js
+++ FrontEnd/screens/HelpScreen.js
@@ -1,5 +1,5 @@
 import React ,{ useState }from 'react';
-import { View, Text, Button, StyleSheet, BackHandler, Alert } from 'react-native';
+import {  Text,  StyleSheet , SafeAreaView} from 'react-native';
 
 // const HelpScreen = () => {
 //   const titleText = useState("Bird's Nest");
@@ -27,37 +27,30 @@ import { View, Text, Button, StyleSheet, BackHandler, Alert } from 'react-native
 //     justifyContent: 'center'
 //   },
 // });
-const HelpScreen = () => {
-  const titleText = useState("Welcome to UAsk");
+const  HelpScreen = (props) => {
   
-  const bodyText = useState("UAsk provides you with answers for questions you ask about an image.");
-  const bodyText1 = useState("To use this app , simply take a picture using your phone's camera or upload an image from your camera roll and start asking questions about the image.");  
-  const bodyText2 = useState("You can use your voice or keyboard to ask your questions.");
-  const bodyText3 = useState("UAsk is meant to assist and empower visuallly impaired individuals in distinguishing explicit  details about an image of their choice.");
 
   return (
-    <Text style={styles.baseText}>
-      <Text style={styles.titleText}>
-      {"\n"}
-        {titleText}
-        {"\n"}
-        {"\n"}
-      </Text>
-      <Text numberOfLines={60}>
-        {bodyText}
-        {"\n"}
-        {"\n"}
-        {bodyText1}
-        {"\n"}
-        {"\n"}
-        {bodyText2}
-        {"\n"}
-        {"\n"}
-        {bodyText3}</Text>
+    
+    <SafeAreaView style={{flex: 1, backgroundColor: '#264CAD'}}>
+    
+    <Text style={styles.titleText}>
+    Welcome 
+    <Text style={styles.innerText}> to UAsk</Text>
     </Text>
+    <Text style={{marginLeft: 35,marginRight:35,  marginTop: 15, fontSize: 20, color: '#fff'}}>
+      {"\n"}UAsk provides you with answers for questions you ask about an image. {"\n"}
+      {"\n"}To use this app , simply take a picture using your phone's camera or upload an image from your camera roll and start asking questions about the image. {"\n"}
+      {"\n"}You can use your voice or keyboard to {"\n"}ask your questions. {"\n"}
+      {"\n"}UAsk is meant to assist and empower visuallly impaired individuals in distinguishing explicit  details about an image of their choice. {"\n"}
+       </Text>
+       
+  
+  </SafeAreaView>
+    
   );
+  
 };
-
 export default HelpScreen;
 
 const styles = StyleSheet.create({
@@ -75,6 +68,16 @@ const styles = StyleSheet.create({
     textAlign:"center"
     
   },
+   ////
+   titleText: {
+    marginLeft: 35,marginTop: 25, fontSize: 30, color: '#fff',
+    fontWeight: 'bold'
+  },
+  innerText: {
+    color: '#fff'
+  },
+  ///
+
   bodyText:{
     fontSize: 1000,
     fontFamily: "Cochin",
@@ -83,5 +86,30 @@ const styles = StyleSheet.create({
 
   }
 });
+// export default HelpScreen;
+//  const styles = StyleSheet.create({}) 
+//   titleText: {
+//     fontSize: 30,
+//     marginBottom: 10,
+    
+//     fontWeight: "bold",
+//     color:"#264CAD",
+//     textAlign:"center"
+    
+//   },
+//   ////
+//    titleText: {
+//     marginLeft: 35,marginTop: 25, fontSize: 30, color: '#fff',
+//     fontWeight: 'bold'
+//   },
+//   innerText: {
+//     color: '#fff'
+//   },
+//   ///
+//   bodyText:{
+//     fontSize: 1000,
+//     fontFamily: "Cochin"
+//   }
+// });
 
 
